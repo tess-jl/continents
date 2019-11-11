e4fdd7b44e7e95879a05111efb8610f540a32dd8
JMoneymaker <jasmin.moneymaker@gmail.com>
2019-11-11 09:46:25 -0800

    add current server.js

--- server.js
+++ server.js
@@ -18,22 +18,45 @@ const PORT = process.env.PORT;
 app.use(morgan('dev')); // http logging
 app.use(cors()); // enable CORS request
 app.use(express.static('./public')); // server files from /public folder
+app.use(express.json());
 
 // API Routes
 
 app.get('/api/continents', async(req, res) => {
 
+
     try {
         const result = await client.query(`
-            SELECT
-                id,
-                name,
-                hemisphere,
-                img,
-                num_countries as "numCountries",
-                fifty_plus_countries as "fiftyPlusCountries"
-            FROM CONTINENTS;
+        SELECT
+            c.*,
+            h.name as hemisphere
+            FROM continents c
+            JOIN hemispheres h
+            ON c.hemisphere_id = h.id
+            ORDER BY c.num_countries;
         `);
+
+        res.json(result.rows);
+
+    }    
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.post('/api/continents', async(req, res)=> {
+    const continent = req.body;
+
+    try {
+        const result = await client.query(`
+            INSERT INTO continents (name, hemiphere_id, img, num_countries, fifty_plus_countries)
+            VALUES ($1, $2, $3, $4, $5)
+            RETURNING *;
+        `,
+        [continent.name, continent.hemisphere_id, continent.img, continent.num_countries, continent.fifty_plus_countries]
+        );
 
         res.json(result.rows);
     }
@@ -43,6 +66,23 @@ app.get('/api/continents', async(req, res) => {
         });
     }
 
+});
+
+app.get('/api/hemispheres', async(req, res) => {
+    try {
+        const result = await client.query(`
+        SELECT * 
+        FROM hemispheres
+        ORDER BY name; 
+        `);
+
+        res.json(result.rows);
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
 });
 
 // Start the server
