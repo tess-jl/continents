9cc12564737307fe65b04fedd46b3c63f6b46127
JMoneymaker <jasmin.moneymaker@gmail.com>
2019-11-11 09:39:05 -0800

    added detail html

---
+++ public/continent-detail.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Famous Cats</title>
+    <link rel="stylesheet" href="common/reset.css">
+    <link rel="stylesheet" href="common/main.css">
+    <link rel="stylesheet" href="common/Header.css">
+    <link rel="stylesheet" href="common/Loading.css">
+    <link rel="stylesheet" href="cat-detail/CatDetailApp.css">
+    <link rel="stylesheet" href="cat-detail/CatDetail.css">
+</head>
+<body>
+    <script type="module" src="cat-detail/cat-detail.js"></script>
+</body>
+</html>
